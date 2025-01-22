c5e6be02536800202bd291cd19238e1584574589
F4ruk.seker <ahmetfaruk731@gmail.com>
2025-01-22 09:54:22 +0300

    Rm Comment

--- testimonial/models.py
+++ testimonial/models.py
@@ -4,7 +4,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Testimonial(models.Model):
     message = models.CharField(max_length=500)
-    # EXTİRME KODA gerek yok mk | yok filed parse edicen de ohooo geç
     name = models.CharField(max_length=75)
 
     phone = PhoneNumberField(default='',  blank=True, null=True)
