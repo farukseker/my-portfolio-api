3a184471bbc833df4679f59fc0bae19d31c8dda1
F4ruk.seker <ahmetfaruk731@gmail.com>
2025-01-27 07:13:45 +0300

    Add Performance tracker

--- pages/api/views/custom_home_view.py
+++ pages/api/views/custom_home_view.py
@@ -5,21 +5,31 @@ from testimonial.models import Testimonial
 from pages.api.serializers import CustomHomeSerializer
 from pages.models import PageModel
 from analytical.utils import ViewCountWithRule
+from performance import get_performance_metric
 
 
 class CustomHomeView(APIView):
     serializer_class = CustomHomeSerializer
 
+    @get_performance_metric
     def get_queryset(self):
         page: PageModel = PageModel.objects.filter(pk='home').first()
         view = ViewCountWithRule(page, self.request)
         view = view()
         return {
-            "featured_projects": ContentModel.objects.filter(show=True, content_type__name='project', is_featured=True),
-            "featured_blogs": ContentModel.objects.filter(show=True, content_type__name='blog', is_featured=True),
-            "testimonials": Testimonial.objects.filter(approved=True),
+            "featured_projects": self.content_loader(show=True, content_type__name='project', is_featured=True),
+            "featured_blogs": self.content_loader(show=True, content_type__name='blog', is_featured=True),
+            "testimonials": self.get_testimonials(),
             "ticket": view.id
         }
+
+    @staticmethod
+    def content_loader(**kwargs):
+        return ContentModel.objects.filter(**kwargs)
+
+    @staticmethod
+    def get_testimonials():
+        return Testimonial.objects.filter(approved=True)
 
     def get(self, request, *args, **kwargs):
         return Response(self.serializer_class(self.get_queryset()).data)
