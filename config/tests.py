ae8b5d47e88b69d235f2cd0b04c18404aa9ecc21
F4ruk.seker <ahmetfaruk731@gmail.com>
2025-04-29 22:59:30 +0300

    Fix custom page view

--- config/tests.py
+++ config/tests.py
@@ -1,11 +1,11 @@
-
-
-from config.utils import EncryptionService
-import json
-
-
-with open('../data.json', 'r', encoding='utf-8') as df:
-    data = json.loads(df.read())
-
-for _ in data:
-    print(EncryptionService().encrypt_data(_))
+#
+#
+# from config.utils import EncryptionService
+# import json
+#
+#
+# with open('../data.json', 'r', encoding='utf-8') as df:
+#     data = json.loads(df.read())
+#
+# for _ in data:
+#     print(EncryptionService().encrypt_data(_))
--- pages/api/serializers/custom_home_serializer.py
+++ pages/api/serializers/custom_home_serializer.py
@@ -1,11 +1,6 @@
 from rest_framework.serializers import Serializer
 from rest_framework.serializers import IntegerField
-from projects.api.serializers import ContentSerializer
-from testimonial.api.serializers import TestimonialSerializer
 
 
 class CustomHomeSerializer(Serializer):
-    featured_projects = ContentSerializer(many=True)
-    featured_blogs = ContentSerializer(many=True)
-    testimonials = TestimonialSerializer(many=True)
     ticket = IntegerField()
