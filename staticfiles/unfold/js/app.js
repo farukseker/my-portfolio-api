a3cbdbd3c1876db6ac8bad86896fdefefe372301
F4ruk.seker <ahmetfaruk731@gmail.com>
2025-12-17 00:53:41 +0300

    add stc

--- staticfiles/unfold/js/app.js
+++ staticfiles/unfold/js/app.js
@@ -1,6 +1,4 @@
 window.addEventListener("load", (e) => {
-  submitSearch();
-
   fileInputUpdatePath();
 
   dateTimeShortcutsOverlay();
@@ -11,6 +9,251 @@ window.addEventListener("load", (e) => {
 
   warnWithoutSaving();
 });
+
+/*************************************************************
+ * Alpine.sort.js callback after sorting
+ *************************************************************/
+const sortRecords = (e) => {
+  const orderingField = e.from.dataset.orderingField;
+
+  const weightInputs = Array.from(
+    e.from.querySelectorAll(
+      `.has_original input[name$=-${orderingField}], td.field-${orderingField} input[name$=-${orderingField}]`
+    )
+  );
+
+  weightInputs.forEach((input, index) => {
+    input.value = index;
+  });
+};
+
+/*************************************************************
+ * Search form
+ *************************************************************/
+function searchForm() {
+  return {
+    applyShortcut(event) {
+      if (
+        event.key === "/" &&
+        document.activeElement.tagName.toLowerCase() !== "input" &&
+        document.activeElement.tagName.toLowerCase() !== "textarea" &&
+        !document.activeElement.isContentEditable
+      ) {
+        event.preventDefault();
+        this.$refs.searchInput.focus();
+      }
+    },
+  };
+}
+
+/*************************************************************
+ * Search dropdown
+ *************************************************************/
+function searchDropdown() {
+  return {
+    openSearchResults: false,
+    currentIndex: 0,
+    applyShortcut(event) {
+      if (
+        event.key === "t" &&
+        document.activeElement.tagName.toLowerCase() !== "input" &&
+        document.activeElement.tagName.toLowerCase() !== "textarea" &&
+        !document.activeElement.isContentEditable
+      ) {
+        event.preventDefault();
+        this.$refs.searchInput.focus();
+      }
+    },
+    nextItem() {
+      if (this.currentIndex < this.maxItem()) {
+        this.currentIndex++;
+      }
+    },
+    prevItem() {
+      if (this.currentIndex > 1) {
+        this.currentIndex--;
+      }
+    },
+    maxItem() {
+      return document.getElementById("search-results").querySelectorAll("li")
+        .length;
+    },
+    selectItem() {
+      const href = this.items[this.currentIndex - 1].querySelector("a").href;
+      window.location = href;
+    },
+  };
+}
+
+/*************************************************************
+ * Search command
+ *************************************************************/
+function searchCommand() {
+  return {
+    el: document.getElementById("command-results"),
+    items: undefined,
+    hasResults: false,
+    openCommandResults: false,
+    currentIndex: 0,
+    totalItems: 0,
+    commandHistory: JSON.parse(localStorage.getItem("commandHistory") || "[]"),
+    handleOpen() {
+      this.openCommandResults = true;
+      this.toggleBodyOverflow();
+      setTimeout(() => {
+        this.$refs.searchInputCommand.focus();
+      }, 20);
+
+      this.items = document.querySelectorAll("#command-history li");
+      this.totalItems = this.items.length;
+    },
+    handleShortcut(event) {
+      if (
+        event.key === "k" &&
+        (event.metaKey || event.ctrlKey) &&
+        document.activeElement.tagName.toLowerCase() !== "input" &&
+        document.activeElement.tagName.toLowerCase() !== "textarea" &&
+        !document.activeElement.isContentEditable
+      ) {
+        event.preventDefault();
+        this.handleOpen();
+      }
+    },
+    handleEscape() {
+      if (this.$refs.searchInputCommand.value === "") {
+        this.toggleBodyOverflow();
+        this.openCommandResults = false;
+        this.el.innerHTML = "";
+        this.items = undefined;
+        this.totalItems = 0;
+        this.currentIndex = 0;
+      } else {
+        this.$refs.searchInputCommand.value = "";
+      }
+    },
+    handleContentLoaded(event) {
+      if (
+        event.target.id !== "command-results" &&
+        event.target.id !== "command-results-list"
+      ) {
+        return;
+      }
+
+      const commandResultsList = document.getElementById(
+        "command-results-list"
+      );
+      if (commandResultsList) {
+        this.items = commandResultsList.querySelectorAll("li");
+        this.totalItems = this.items.length;
+      } else {
+        this.items = undefined;
+        this.totalItems = 0;
+      }
+
+      if (event.target.id === "command-results") {
+        this.currentIndex = 0;
+
+        if (this.items) {
+          this.totalItems = this.items.length;
+        } else {
+          this.totalItems = 0;
+        }
+      }
+
+      this.hasResults = this.totalItems > 0;
+
+      if (!this.hasResults) {
+        this.items = document.querySelectorAll("#command-history li");
+      }
+    },
+    handleOutsideClick() {
+      this.$refs.searchInputCommand.value = "";
+      this.openCommandResults = false;
+      this.toggleBodyOverflow();
+    },
+    toggleBodyOverflow() {
+      document
+        .getElementsByTagName("body")[0]
+        .classList.toggle("overflow-hidden");
+    },
+    scrollToActiveItem() {
+      const item = this.items[this.currentIndex - 1];
+
+      if (item) {
+        item.scrollIntoView({
+          behavior: "smooth",
+          block: "nearest",
+        });
+      }
+    },
+    nextItem() {
+      if (this.currentIndex < this.totalItems) {
+        this.currentIndex++;
+        this.scrollToActiveItem();
+      }
+    },
+    prevItem() {
+      if (this.currentIndex > 1) {
+        this.currentIndex--;
+        this.scrollToActiveItem();
+      }
+    },
+    selectItem(addHistory) {
+      const link = this.items[this.currentIndex - 1].querySelector("a");
+      const data = {
+        title: link.dataset.title,
+        description: link.dataset.description,
+        link: link.href,
+        favorite: false,
+      };
+
+      if (addHistory) {
+        this.addToHistory(data);
+      }
+
+      window.location = link.href;
+    },
+    addToHistory(data) {
+      let commandHistory = JSON.parse(
+        localStorage.getItem("commandHistory") || "[]"
+      );
+
+      for (const [index, item] of commandHistory.entries()) {
+        if (item.link === data.link) {
+          commandHistory.splice(index, 1);
+        }
+      }
+
+      commandHistory.unshift(data);
+      commandHistory = commandHistory.slice(0, 10);
+      this.commandHistory = commandHistory;
+      localStorage.setItem("commandHistory", JSON.stringify(commandHistory));
+    },
+    removeFromHistory(event, index) {
+      event.preventDefault();
+
+      const commandHistory = JSON.parse(
+        localStorage.getItem("commandHistory") || "[]"
+      );
+      commandHistory.splice(index, 1);
+      this.commandHistory = commandHistory;
+      localStorage.setItem("commandHistory", JSON.stringify(commandHistory));
+    },
+    toggleFavorite(event, index) {
+      event.preventDefault();
+
+      const commandHistory = JSON.parse(
+        localStorage.getItem("commandHistory") || "[]"
+      );
+
+      commandHistory[index].favorite = !commandHistory[index].favorite;
+      this.commandHistory = commandHistory.sort(
+        (a, b) => Number(b.favorite) - Number(a.favorite)
+      );
+      localStorage.setItem("commandHistory", JSON.stringify(commandHistory));
+    },
+  };
+}
 
 /*************************************************************
  * Warn without saving
@@ -24,9 +267,11 @@ const warnWithoutSaving = () => {
       "form.warn-unsaved-form input, form.warn-unsaved-form select, form.warn-unsaved-form textarea"
     );
 
-    Array.from(elements).forEach((field) => {
-      field.addEventListener("input", (e) => (formChanged = true));
-    });
+    for (const field of elements) {
+      field.addEventListener("input", () => {
+        formChanged = true;
+      });
+    }
   };
 
   if (!form) {
@@ -94,7 +339,7 @@ const watchClassChanges = (selector, callback) => {
  *************************************************************/
 const dateTimeShortcutsOverlay = () => {
   const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutationRecord) => {
+    for (const mutationRecord of mutations) {
       const display = mutationRecord.target.style.display;
       const overlay = document.getElementById("modal-overlay");
 
@@ -103,79 +348,50 @@ const dateTimeShortcutsOverlay = () => {
       } else {
         overlay.style.display = "none";
       }
-    });
+    }
   });
 
   const targets = document.querySelectorAll(".calendarbox, .clockbox");
 
-  Array.from(targets).forEach((target) => {
+  for (const target of targets) {
     observer.observe(target, {
       attributes: true,
       attributeFilter: ["style"],
     });
-  });
+  }
 };
 
 /*************************************************************
  * File upload path
  *************************************************************/
 const fileInputUpdatePath = () => {
-  Array.from(document.querySelectorAll("input[type=file]")).forEach((input) => {
-    input.addEventListener("change", (e) => {
-      const parts = e.target.value.split("\\");
-      const placeholder =
-        input.parentNode.parentNode.querySelector("input[type=text]");
-      placeholder.setAttribute("value", parts[parts.length - 1]);
-    });
-  });
-};
+  const checkInputChanged = () => {
+    for (const input of document.querySelectorAll("input[type=file]")) {
+      if (input.hasChangeListener) {
+        continue;
+      }
 
-/*************************************************************
- * Search form on changelist view
- *************************************************************/
-const submitSearch = () => {
-  const searchbar = document.getElementById("searchbar");
-  const searchbarSubmit = document.getElementById("searchbar-submit");
+      input.addEventListener("change", (e) => {
+        const parts = e.target.value.split("\\");
+        const placeholder =
+          input.parentNode.parentNode.parentNode.querySelector(
+            "input[type=text]"
+          );
+        placeholder.setAttribute("value", parts[parts.length - 1]);
+      });
 
-  const getQueryParams = (searchString) => {
-    const queryParams = window.location.search
-      .replace("?", "")
-      .split("&")
-      .map((param) => param.split("="))
-      .reduce((values, [key, value]) => {
-        if (key && key !== "q") {
-          values[key] = value;
-        }
-
-        return values;
-      }, {});
-
-    if (searchString) {
-      queryParams["q"] = searchString;
+      input.hasChangeListener = true;
     }
-
-    const result = Object.entries(queryParams)
-      .map(([key, value]) => `${key}=${value}`)
-      .join("&");
-
-    return `?${result}`;
   };
 
-  if (searchbar !== null) {
-    searchbar.addEventListener("keypress", (e) => {
-      if (e.key === "Enter") {
-        window.location = getQueryParams(e.target.value);
-        e.preventDefault();
-      }
-    });
-  }
+  new MutationObserver(() => {
+    checkInputChanged();
+  }).observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
 
-  if (searchbarSubmit !== null && searchbar !== null) {
-    searchbarSubmit.addEventListener("click", (e) => {
-      e.preventDefault();
-      window.location = getQueryParams(searchbar.value);
-    });
-  }
+  checkInputChanged();
 };
 
 /*************************************************************
@@ -202,6 +418,12 @@ const DEFAULT_CHART_OPTIONS = {
       pointBorderWidth: 0,
       pointStyle: false,
     },
+    pie: {
+      borderWidth: 0,
+    },
+    doughnut: {
+      borderWidth: 0,
+    },
   },
   plugins: {
     legend: {
@@ -222,6 +444,13 @@ const DEFAULT_CHART_OPTIONS = {
   },
   scales: {
     x: {
+      display: function (context) {
+        if (["pie", "doughnut", "radar"].includes(context.chart.config.type)) {
+          return false;
+        }
+
+        return true;
+      },
       border: {
         dash: [5, 5],
         dashOffset: 2,
@@ -230,6 +459,11 @@ const DEFAULT_CHART_OPTIONS = {
       ticks: {
         color: "#9ca3af",
         display: true,
+        maxTicksLimit: function (context) {
+          return context.chart.data.datasets.find(
+            (dataset) => dataset.maxTicksXLimit
+          )?.maxTicksXLimit;
+        },
       },
       grid: {
         display: true,
@@ -237,23 +471,42 @@ const DEFAULT_CHART_OPTIONS = {
       },
     },
     y: {
+      display: function (context) {
+        if (["pie", "doughnut", "radar"].includes(context.chart.config.type)) {
+          return false;
+        }
+
+        return true;
+      },
       border: {
         dash: [5, 5],
         dashOffset: 5,
         width: 0,
       },
       ticks: {
-        display: false,
-        font: {
-          size: 13,
+        color: "#9ca3af",
+        display: function (context) {
+          return context.chart.data.datasets.some((dataset) => {
+            return (
+              dataset.hasOwnProperty("displayYAxis") && dataset.displayYAxis
+            );
+          });
+        },
+        callback: function (value) {
+          const suffix = this.chart.data.datasets.find(
+            (dataset) => dataset.suffixYAxis
+          )?.suffixYAxis;
+          if (suffix) {
+            return `${value} ${suffix}`;
+          }
+          return value;
         },
       },
       grid: {
-        lineWidth: function (context) {
+        lineWidth: (context) => {
           if (context.tick.value === 0) {
             return 1;
           }
-
           return 0;
         },
         tickWidth: 0,
@@ -263,38 +516,104 @@ const DEFAULT_CHART_OPTIONS = {
 };
 
 const renderCharts = () => {
-  let charts = [];
+  const charts = [];
 
   const changeDarkModeSettings = () => {
     const hasDarkClass = document
       .querySelector("html")
       .classList.contains("dark");
 
-    charts.forEach((chart) => {
-      chart.options.scales.x.grid.color = hasDarkClass ? "#374151" : "#d1d5db";
-      chart.options.scales.y.grid.color = hasDarkClass ? "#374151" : "#d1d5db";
+    const baseColorDark = getComputedStyle(document.documentElement)
+      .getPropertyValue("--color-base-700")
+      .trim();
+
+    const baseColorLight = getComputedStyle(document.documentElement)
+      .getPropertyValue("--color-base-300")
+      .trim();
+
+    const borderColor = hasDarkClass ? baseColorDark : baseColorLight;
+
+    for (const chart of charts) {
+      if (chart.options.scales.x) {
+        chart.options.scales.x.grid.color = borderColor;
+      }
+
+      if (chart.options.scales.y) {
+        chart.options.scales.y.grid.color = borderColor;
+      }
+
+      if (chart.options.scales.r) {
+        chart.options.scales.r.grid.color = borderColor;
+      }
       chart.update();
-    });
+    }
   };
 
-  Array.from(document.querySelectorAll(".chart")).forEach((chart) => {
+  for (const chart of document.querySelectorAll(".chart")) {
     const ctx = chart.getContext("2d");
     const data = chart.dataset.value;
     const type = chart.dataset.type;
     const options = chart.dataset.options;
 
     if (!data) {
-      return;
+      continue;
     }
+
+    const parsedData = JSON.parse(chart.dataset.value);
+
+    for (const key in parsedData.datasets) {
+      const dataset = parsedData.datasets[key];
+      const processColor = (colorProp) => {
+        if (Array.isArray(dataset?.[colorProp])) {
+          for (const [index, prop] of dataset?.[colorProp].entries()) {
+            if (prop.startsWith("var(")) {
+              const cssVar = prop.match(/var\((.*?)\)/)[1];
+              const color = getComputedStyle(document.documentElement)
+                .getPropertyValue(cssVar)
+                .trim();
+              dataset[colorProp][index] = color;
+            }
+          }
+        } else if (dataset?.[colorProp]?.startsWith("var(")) {
+          const cssVar = dataset[colorProp].match(/var\((.*?)\)/)[1];
+          const color = getComputedStyle(document.documentElement)
+            .getPropertyValue(cssVar)
+            .trim();
+          dataset[colorProp] = color;
+        }
+      };
+
+      processColor("borderColor");
+      processColor("backgroundColor");
+    }
+
+    CHART_OPTIONS = { ...DEFAULT_CHART_OPTIONS };
+    if (type === "radar") {
+      CHART_OPTIONS.scales = {
+        r: {
+          ticks: {
+            backdropColor: "transparent",
+          },
+          pointLabels: {
+            color: "#9ca3af",
+            font: {
+              size: 12,
+            },
+          },
+        },
+      };
+    }
+    Chart.defaults.font.family = "Inter";
+    Chart.defaults.font.size = 12;
 
     charts.push(
       new Chart(ctx, {
         type: type || "bar",
-        data: JSON.parse(chart.dataset.value),
-        options: options ? JSON.parse(options) : DEFAULT_CHART_OPTIONS,
+        data: parsedData,
+        options: options ? JSON.parse(options) : { ...CHART_OPTIONS },
       })
     );
-  });
+  }
 
   changeDarkModeSettings();
 
